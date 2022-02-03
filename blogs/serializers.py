3ce53f3d2977e282be54b9a406748c0cc596baab
Paqimm <mcgeewilliam815@gmail.com>
2022-02-03 16:38:13 +0300

    chore (Endpoint) : add Comment models, serializers, views and urls

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -1,7 +1,12 @@
-from blogs.models import Blog
 from rest_framework import serializers
+from blogs.models import Blog, Comment
 
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model= Blog
         fields=['id','blog_created_at','blog_read_time','blog_title','blog_content']
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Comment
+        fields=['id','comment_created_at','comment_content']
