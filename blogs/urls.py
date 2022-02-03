cd82a6fab8f082c7c92f6d3aab5e5049142618ac
Paqimm <mcgeewilliam815@gmail.com>
2022-02-03 16:44:34 +0300

    refractor (URL Path) : use comments instead of comment

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,6 +4,6 @@ from blogs.views import BlogAPIView, BlogDetailAPIView,CommentAPIView,CommentDet
 urlpatterns= [
     path('blogs/',BlogAPIView.as_view(),name='blogs'),
     path('blogs/<int:pk>/',BlogDetailAPIView.as_view(),name='blog-details'),
-    path('comment/',CommentAPIView.as_view(),name='comment'),
-    path('comment/<int:pk>/',CommentDetailAPIView.as_view(),name='comment-details'),
+    path('comments/',CommentAPIView.as_view(),name='comment'),
+    path('comments/<int:pk>/',CommentDetailAPIView.as_view(),name='comment-details'),
 ]
