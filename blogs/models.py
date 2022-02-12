3362e2c46b5b310a4bc8c9dbad2a9a52b28824df
Paqimm <mcgeewilliam815@gmail.com>
2022-02-12 13:18:17 +0300

    chore (Blog) : remove comment

--- blogs/models.py
+++ blogs/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Blog(models.Model):
     blog_title= models.CharField(max_length=255)
     blog_content= models.TextField()
-    # blog_read_time= models.IntegerField(default=10)
     blog_created_at= models.DateTimeField(auto_now_add=True)
     blog_updated_at= models.DateTimeField(auto_now=True)
     blog_owner= models.ForeignKey('authentication.User',related_name='blogs',on_delete=models.CASCADE)
