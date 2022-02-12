60cbbc4eb74a60ef288c3d1b093d033c292423f7
Paqimm <mcgeewilliam815@gmail.com>
2022-02-12 13:40:48 +0300

    refractor (User) : show user instead of user ID

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -4,7 +4,8 @@ from rest_framework import serializers
 
 class BlogSerializer(serializers.ModelSerializer):
     comments= serializers.PrimaryKeyRelatedField(many=True,read_only=True)
+    blog_owner= serializers.ReadOnlyField(source='blog_owner.username')
     class Meta:
         model= Blog
         fields=['id','blog_owner','blog_created_at','blog_read_time','blog_title','blog_content','comments']
-        read_only_fields= ['blog_owner']
+        # read_only_fields= ['blog_owner']
--- comments/serializers.py
+++ comments/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from comments.models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
+    comment_owner= serializers.ReadOnlyField(source='comment_owner.username')
     class Meta:
         model= Comment
         fields=['id','comment_created_at','comment_owner','comment_under','comment_content']
-        read_only_fields= ['comment_owner']
+        # read_only_fields= ['comment_owner']
